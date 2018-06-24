92c679f92d849a727cb32c78518214254df37955
Marquis Kurt <software@marquiskurt.net>
2018-06-24 10:51:28 -1000

    Fix JSON bug, ad new CSS

--- assets/js/getRelease.js
+++ assets/js/getRelease.js
@@ -1,13 +1,13 @@
 function getRelease()
 {
-    $.getJSON("version.json",{}, function( data ){
+    $.getJSON("release.json",{}, function( data ){
         var releaseState = data.release.state;
         var releaseVersion = data.release.version;
         var releaseLink = data.release.link;
 
         try {
             var dl = document.getElementById('dl-button');
-            dl.innerHTML = "Download" + releaseState + " (" + releaseVersion + ")";
+            dl.innerHTML = "Download " + releaseState + " (" + releaseVersion + ")";
             $('#dl-button').attr('href', releaseLink);
         } catch (error) {
             console.error("Couldn't get data.");
