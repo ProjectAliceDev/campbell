9a381c0603540e76fd948ca1878d778c647d2058
Marquis Kurt <software@marquiskurt.net>
2018-06-25 21:15:25 -1000

    Finish off the initial Campbell project

--- assets/js/getRelease.js
+++ assets/js/getRelease.js
@@ -8,9 +8,38 @@ function getRelease()
         try {
             var dl = document.getElementById('dl-button');
             dl.innerHTML = "Download " + releaseState + " (" + releaseVersion + ")";
-            $('#dl-button').attr('href', releaseLink);
+            if (releaseLink == "None") {
+                $('#dl-button').addClass('is--disabled');
+                $('#dl-button').attr('aria-disabled', true);
+            } else {
+                $('#dl-button').attr('href', releaseLink);
+            }
+            
         } catch (error) {
             console.error("Couldn't get data.");
         }
     });
+}
+
+function getAliceRelease()
+{
+    $.getJSON("release.json", {}, function( data ){
+        var aliceosState = data.aliceos.state;
+        var aliceosVersion = data.aliceos.version;
+        var aliceosLink = data.aliceos.link;
+
+        try {
+            var adl = document.getElementById('aliceos-dl-button');
+            adl.innerHTML = "Download " + aliceosState + " (" + aliceosVersion + ")";
+            if (aliceosLink == "None") {
+                $('#aliceos-dl-button').addClass('is--disabled');
+                $('#aliceos-dl-button').attr('aria-disabled', true);
+            } else {
+                $('#aliceis-dl-button').attr('href', aliceosLink);
+            }
+            
+        } catch (error) {
+            console.error("Couldn't get data")
+        }
+    })
 }
