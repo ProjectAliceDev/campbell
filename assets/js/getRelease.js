238e62a478bdd8ce6390ea7f1846019720761e83
Marquis Kurt <software@marquiskurt.net>
2018-07-05 15:57:10 -1000

    Massize site overhaul!

--- assets/js/getRelease.js
+++ assets/js/getRelease.js
@@ -3,16 +3,35 @@ function getRelease()
     $.getJSON("release.json",{}, function( data ){
         var releaseState = data.release.state;
         var releaseVersion = data.release.version;
-        var releaseLink = data.release.link;
+        var releaseDl = data.release.can_download;
+
+        var betaVersion = data.beta.version;
+        var betaBuild = data.beta.build;
+        var betaDl = data.beta.can_download;
+
+        var baseURL = "https://github.com/TheAngelReturns/the-angel-returns/releases/tag/";
 
         try {
             var dl = document.getElementById('dl-button');
             dl.innerHTML = "Download " + releaseState + " (" + releaseVersion + ")";
-            if (releaseLink == "None") {
+            if (releaseDl == "False") {
                 $('#dl-button').addClass('is--disabled');
                 $('#dl-button').attr('aria-disabled', true);
             } else {
-                $('#dl-button').attr('href', releaseLink);
+                $('#dl-button').attr('href', baseURL + releaseVersion);
+            }
+            
+        } catch (error) {
+            console.error("Couldn't get data.");
+        }
+
+        try {
+            var beta = document.getElementById('dl-button-beta');
+            beta.innerHTML = "Download " + betaVersion + " Beta " + betaBuild;
+            if (betaDl == "False") {
+                $('#dl-button-beta').hide();
+            } else {
+                $('#dl-button-beta').attr('href', baseURL + betaVersion + "beta" + betaBuild);
             }
             
         } catch (error) {
